efb865e3ded53608c1af81477199391fdf31194b
Thiago Rocha Lira <rochalirathiago@outlook.com>
2026-03-08 21:44:58 -0300

    Tabuleiro atualizado com lógica de preenchimento com números.

--- aula04/main.c
+++ aula04/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void iniciarTabuleiro(char tabuleiro[3][3]){
+    int k = 1;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            tabuleiro[i][j] = 'O';
+            tabuleiro[i][j] = '0' + k;
+            k++;
         }
     }
 }
@@ -22,11 +24,20 @@ void desenharTabuleiro(char tabuleiro[3][3]){
     }
 }
 
+// Em trabalho
+void popularCasa(char tabuleiro[3][3], int posicaoX, int posicaoY){
+    if(tabuleiro[posicaoX][posicaoY] != ' '){
+        printf("Campo já ocupado\n");
+    }
+}
+
 int main(){
     char tabuleiro[3][3];
-    
+
     iniciarTabuleiro(tabuleiro);
     desenharTabuleiro(tabuleiro);
+
+    popularCasa(tabuleiro, 1, 1);
 
     return 0;
 }
